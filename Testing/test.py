ba7e08c4512e8c856019e824e8f4ec0110776bb4
pavelsheludkov <TMqm9Dg7c@9$H3ro>
2021-03-29 22:03:33 +1000

    Advanced looping

--- Testing/test.py
+++ Testing/test.py
@@ -1,5 +1,18 @@
-print("""You can print
-on multiple
-lines using
-triple
-quotes.""")
+for i in range(1,10):
+    for js in range(10-i):
+        print(" ", end=" ")
+    for j in range(1,i+1):
+        print(j, end=" ")
+    for k in range(i-1,0,-1):
+        print(k, end=" ")
+    for ks in range(10-i):
+        print(" ", end=" ")
+    print()
+for i in range(9,1,-1):
+    for js in range (11-i):
+        print(" ", end=" ")
+    for j in range(1, i-1):
+        print(j, end=" ")
+    for k in range(i-1,0,-1):
+        print(k, end=" ")
+    print()
